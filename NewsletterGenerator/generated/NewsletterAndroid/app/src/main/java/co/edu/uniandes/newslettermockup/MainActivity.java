37aea5f4a447c976cf428d0edc3b043085333aa9
Diego Salinas <diegoss.andres@gmail.com>
2016-05-18 15:36:38 -0500

    Primera version de la transformacion ETL Newsletter to Androd. Falta resolver problemas con colores.

--- NewsletterGenerator/generated/NewsletterAndroid/app/src/main/java/co/edu/uniandes/newslettermockup/MainActivity.java
+++ NewsletterGenerator/generated/NewsletterAndroid/app/src/main/java/co/edu/uniandes/newslettermockup/MainActivity.java
@@ -27,12 +27,6 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 		actionLinksViews = new HashMap<Integer, String>();
 		
 		//Por cada vista con link se crea un registro en un hashmap de actions
-		actionLinksViews.put(R.id.imageView_disenoboletin19febrerob02_2, "https://apps.co/comunicaciones/articulos/elementos-legales-para-el-emprendimiento-digital-2/?mkt_hm=4&utm_source=email_marketing&utm_admin=65089&utm_medium=email&utm_campaign=Elementos");
-		actionLinksViews.put(R.id.button_presencial, "https://apps.co/comunicaciones/eventos/elementos-legales-para-el-emprendimiento-digital-3/");
-		actionLinksViews.put(R.id.button_virtual, "https://plus.google.com/events/c541njr83ht26v8ic03tslq4c3c");
-		actionLinksViews.put(R.id.imageView_disenoboletin19febrero14, "https://apps.co/comunicaciones/articulos/certificate-internacionalmente-como-profesional-ti/");
-		actionLinksViews.put(R.id.layout_video1, "https://www.youtube.com/watch?v=p542clXAwRw");
-		actionLinksViews.put(R.id.layout_video2, "https://www.youtube.com/watch?v=QY02W8XK61c&list=PL1C9B31303DD2150E");
 	}
 	
 	
