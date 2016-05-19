10b2038749372d29ee20d70c5aa26ba23d060e71
Diego Salinas <diegoss.andres@gmail.com>
2016-05-19 01:34:34 -0500

    Creacion representacion HTML. Se deja primera version de la ejecucion completa de la cadena. Android y html (faltaria asociarla con editor) Sigo mañana con jar y copia de archivos estaticos del proyecto

--- NewsletterGenerator/generated/NewsletterAndroid/app/src/main/java/co/edu/uniandes/newslettermockup/MainActivity.java
+++ NewsletterGenerator/generated/NewsletterAndroid/app/src/main/java/co/edu/uniandes/newslettermockup/MainActivity.java
@@ -27,16 +27,15 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 		actionLinksViews = new HashMap<Integer, String>();
 		
 		//Por cada vista con link se crea un registro en un hashmap de actions
-		actionLinksViews.put(R.id.video_linearLayout_15, "https://www.youtube.com/watch?v=QY02W8XK61c&list=PL1C9B31303DD2150E");
-		actionLinksViews.put(R.id.button_1, "https://apps.co/comunicaciones/eventos/elementos-legales-para-el-emprendimiento-digital-3/");
-		actionLinksViews.put(R.id.button_4, "https://www.youtube.com/watch?v=QY02W8XK61c&list=PL1C9B31303DD2150E");
-		actionLinksViews.put(R.id.button_2, "https://plus.google.com/events/c541njr83ht26v8ic03tslq4c3c");
-		actionLinksViews.put(R.id.imageView_2, "https://apps.co/comunicaciones/articulos/elementos-legales-para-el-emprendimiento-digital-2/?mkt_hm=4&amp;utm_source=email_marketing&amp;utm_admin=65089&amp;utm_medium=email&amp;utm_campaign=Elementos_");
-		actionLinksViews.put(R.id.imageView_1, "http://apps.co");
-		actionLinksViews.put(R.id.imageView_5, "https://apps.co/comunicaciones/articulos/certificate-internacionalmente-como-profesional-ti");
-		actionLinksViews.put(R.id.video_linearLayout_13, "https://www.youtube.com/watch?v=p542clXAwRw");
 		actionLinksViews.put(R.id.button_3, "https://www.youtube.com/watch?v=p542clXAwRw");
-		actionLinksViews.put(R.id.imageView_7, "https://www.youtube.com/watch?v=QY02W8XK61c&list=PL1C9B31303DD2150E");
+		actionLinksViews.put(R.id.imageView_2, "https://apps.co/comunicaciones/articulos/elementos-legales-para-el-emprendimiento-digital-2/?mkt_hm=4&amp;utm_source=email_marketing&amp;utm_admin=65089&amp;utm_medium=email&amp;utm_campaign=Elementos_");
+		actionLinksViews.put(R.id.video_linearLayout_13, "https://www.youtube.com/watch?v=p542clXAwRw");
+		actionLinksViews.put(R.id.imageView_1, "http://apps.co");
+		actionLinksViews.put(R.id.button_1, "https://apps.co/comunicaciones/eventos/elementos-legales-para-el-emprendimiento-digital-3/");
+		actionLinksViews.put(R.id.button_2, "https://plus.google.com/events/c541njr83ht26v8ic03tslq4c3c");
+		actionLinksViews.put(R.id.imageView_5, "https://apps.co/comunicaciones/articulos/certificate-internacionalmente-como-profesional-ti");
+		actionLinksViews.put(R.id.button_4, "https://www.youtube.com/watch?v=QY02W8XK61c&list=PL1C9B31303DD2150E");
+		actionLinksViews.put(R.id.video_linearLayout_15, "https://www.youtube.com/watch?v=QY02W8XK61c&list=PL1C9B31303DD2150E");
 	}
 	
 	
